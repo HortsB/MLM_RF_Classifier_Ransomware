c4570dcf2bb26677fc5b60cc31f0fbafa9d13298
billy <billy006jkr@gmail.com>
2024-11-20 11:47:10 -0500

    Update timer

--- MRF_APRENDIZAJE.py
+++ MRF_APRENDIZAJE.py
@@ -22,7 +22,7 @@ def main():
     X = df.drop(columns=['BitcoinAddresses','Benign'])
     y = df['Benign']
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_state=42)
 
     rF=RandomForestClassifier(random_state=42)
     rF.fit(X_train, y_train)
